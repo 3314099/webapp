8191541e4a630dab2ff025f5cbe6f730f58fdcbc
Andrey Petrov <3314099@mail.ru>
2020-09-28 18:59:18 +0300

    instaled sass

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,13 @@
 const path = require('path')
 const MinifyPlugin = require('babel-minify-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports={
 	mode: 'development',
 	devtool: 'source-map',
 	context: path.resolve(__dirname, 'src'),
-	entry: './index.js',
+	entry: ['./index.js', './style.scss'],
 
 	devServer: {
 		contentBase: path.join(__dirname, 'dist'),
@@ -15,15 +16,35 @@ module.exports={
 	},
 
 	module: {
-		rules: [{
+		rules: [
+			{
 			test: /\.(js|jsx)$/,
 			exclude: /node_modules/,
 			loader: 'babel-loader'
-		}]
+			},
+			{
+			test: /\.(scss|css)/,
+				exclude: /node_modules/,
+				use: [
+					{
+						loader: MiniCssExtractPlugin.loader,
+						options: {
+							reloadAll: true
+						}
+					},
+					'css-loader',
+					'postcss-loader',
+					'sass-loader'
+				]
+			}
+		]
 	},
 	plugins: [
 		new MinifyPlugin({}, {
 			comments: false
+		}),
+		new MiniCssExtractPlugin({
+			filename: '[name].css'
 		}),
 		new HtmlWebpackPlugin({
 			title: 'Home',
